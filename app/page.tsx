069e941978095b7f2105b038383042ca0fa3d3ef
mahran1989 <mahroukmeher@gmail.com>
2024-10-05 21:29:16 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <h1>Bienvenue chez GPAutomobile</h1>
-      <p>Votre source fiable pour les pièces automobiles en Tunisie</p>
-    </main>
-  )
+  return <h1>Bienvenue chez GPAutomobile</h1>
 }
